0fab8fcf8b8bfc28239703f59253c10c2898ad4d
Shuflduf <shuflduf@gmail.com>
2026-01-27 21:14:32 -0700

    more deps i forgot

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 sudo pacman -S \
+  hyprland \
+  polkit-kde-agent \
+  xdg-desktop-portal-hyprland \
+  dunst \
   ghostty \
   zoxide \
   yazi \
@@ -11,10 +15,12 @@ sudo pacman -S \
   lazygit \
   impala \
   hyprsunset \
+  hyprpaper \
   hyprshot \
   hyprpicker \
   hyprlock \
   hyprlauncher \
+  hypridle \
   helix \
   fish \
   eza \
@@ -27,8 +33,17 @@ yay -S \
   pacsea-bin \
   zen-browser-bin
 
-mkdir -p ${XDG_CONFIG_HOME}/fish/
-cp -f ./config.fish $XDG_CONFIG_HOME/fish/
+mkdir -p $XDG_CONFIG_HOME/fish/
+mkdir -p $XDG_CONFIG_HOME/quickshell/
+mkdir -p $XDG_CONFIG_HOME/hypr/
+mkdir -p $XDG_DATA_HOME/Risk-Of-Rice/wallpapers
 
 mkdir -p ~/Pictures/Screenshots
 
+cp -f ./config.fish $XDG_CONFIG_HOME/fish/
+cp -rf ./quickshell/* $XDG_CONFIG_HOME/quickshell/
+cp -f ./hyprland.conf $XDG_CONFIG_HOME/hypr/
+cp -rf ./wallpapers/* $XDG_DATA_HOME/Risk-Of-Rice/wallpapers
+
+chsh -s $(which fish)
+sudo systemctl enable --now power-profiles-daemon
