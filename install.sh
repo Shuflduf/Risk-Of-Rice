f3d2fb38c39cf0ce56b79c74b4a8671bc88c3904
Shuflduf <shuflduf@gmail.com>
2026-01-27 20:38:36 -0700

    started making isntall script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo pacman -S \
+  ghostty \
+  zoxide \
+  yazi \
+  starship \
+  quickshell \
+  pulsemixer \
+  power-profiles-daemon \
+  lazygit \
+  impala \
+  hyprsunset \
+  hyprshot \
+  hyprpicker \
+  hyprlock \
+  hyprlauncher \
+  helix \
+  fish \
+  eza \
+  btop \
+  bluetui \
+  7zip
+
+yay -S \
+  spotify \
+  pacsea-bin
+
